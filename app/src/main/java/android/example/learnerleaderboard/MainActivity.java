fc8013f40189b9c917638fff42872bf9830dcdbc
GozieUdemba <66220321+GozieUdemba@users.noreply.github.com>
2020-09-09 17:47:25 +0100

    completed home screen ui

--- app/src/main/java/android/example/learnerleaderboard/MainActivity.java
+++ app/src/main/java/android/example/learnerleaderboard/MainActivity.java
@@ -1,14 +1,63 @@
 package android.example.learnerleaderboard;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 
+import android.example.learnerleaderboard.databinding.ActivityMainBinding;
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    private String[] tabTitles = new String[]{"Learning Leaders", "Skill IQ Leaders"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        initializeTabs();
+    }
+
+    private void initializeTabs(){
+        binding.viewPager.setAdapter(new ViewPagerFragmentAdapter(this));
+
+        new TabLayoutMediator(binding.tabLayout, binding.viewPager, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                tab.setText(tabTitles[position]);
+            }
+        }).attach();
+    }
+
+    private class ViewPagerFragmentAdapter extends FragmentStateAdapter{
+        public ViewPagerFragmentAdapter(@NonNull FragmentActivity fragment) {
+            super(fragment);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            switch (position){
+                case 0:
+                    return new LearningFragment();
+                case 1:
+                    return new SkillsFragment();
+            }
+            return new LearningFragment();
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 2;
+        }
     }
 }
---
+++ app/src/main/java/android/example/learnerleaderboard/SkillsFragment.java
@@ -0,0 +1,33 @@
+package android.example.learnerleaderboard;
+
+import android.example.learnerleaderboard.databinding.FragmentLearningBinding;
+import android.example.learnerleaderboard.databinding.FragmentSkillsBinding;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+
+public class SkillsFragment extends Fragment {
+    private FragmentSkillsBinding binding;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_skills, container, false);
+        return binding.getRoot();    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
